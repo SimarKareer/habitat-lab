0fcc689a75e450f1de8faca6827c6d6a2bc59bcc
Simar Kareer <skareer@outlook.com>
2021-11-10 15:10:05 -0500

    added friction to cube.  ckpt before adding energt locomotion

---
+++ habitat_baselines/common/aliengo.py
@@ -0,0 +1,219 @@
+import numpy as np
+
+from habitat_baselines.common.baseline_registry import baseline_registry
+import magnum as mn
+from habitat_sim.physics import JointMotorSettings
+from copy import copy
+import math
+
+from habitat.utils.geometry_utils import wrap_heading
+
+class AlienGo:
+    def __init__(self, robot_id, sim, fixed_base, task_config):
+        self.robot_id = robot_id
+        self._sim = sim
+        self.fixed_base = fixed_base
+        self.task_config = task_config
+
+        self.jmsIdxToJoint = [
+            "FL_hip",
+            "FL_thigh",
+            "FL_calf",
+            "FR_hip",
+            "FR_thigh",
+            "FR_calf",
+            "RL_hip",
+            "RL_thigh",
+            "RL_calf",
+            "RR_hip",
+            "RR_thigh",
+            "RR_calf",
+        ]
+
+        # joint position limits
+        self.joint_limits_lower = np.array(
+            [-0.1, -np.pi / 3, -5 / 6 * np.pi] * 4
+        )
+        self.joint_limits_upper = np.array([0.1, np.pi / 2.1, -np.pi / 4] * 4)
+
+    @property
+    def height(self):
+        # Translation is [y, z, x]
+        return self.robot_id.rigid_state.translation[1]
+
+    @property
+    def joint_velocities(self) -> np.ndarray:
+        return np.array(self.robot_id.joint_velocities, dtype=np.float32)
+
+    @property
+    def joint_positions(self) -> np.ndarray:
+        return np.array(self.robot_id.joint_positions, dtype=np.float32)
+
+    def set_joint_positions(self, pose):
+        """This is kinematic! Not dynamic."""
+        self.robot_id.joint_positions = wrap_heading(pose)
+
+    def get_feet_contacts(self):
+        """THIS ASSUMES THAT THERE IS ONLY ONE ROBOT IN THE SIM
+        Returns np.array size 4, either 1s or 0s, for FL FR RL RR feet.
+        """
+        contacts = self._sim.get_physics_contact_points()
+        contacting_feet = set()
+        for c in contacts:
+            for link in [c.link_id_a, c.link_id_b]:
+                contacting_feet.add(self.robot_id.get_link_name(link))
+        return np.array(
+            [
+                1 if foot in contacting_feet else 0
+                for foot in ["FL_foot", "FR_foot", "RL_foot", "RR_foot"]
+            ]
+        )
+
+    def _jms_copy(self, jms):
+        """Returns a deep copy of a jms
+
+        Args:
+            jms: the jms to copy
+        """
+        return JointMotorSettings(
+            jms.position_target,
+            jms.position_gain,
+            jms.velocity_target,
+            jms.velocity_gain,
+            jms.max_impulse,
+        )
+
+    def _new_jms(self, pos):
+        """Returns a new jms with default settings at a given position
+
+        Args:
+            pos: the new position to set to
+        """
+        return JointMotorSettings(
+            pos,  # position_target
+            0.6,  # position_gain
+            0.0,  # velocity_target
+            1.5,  # velocity_gain
+            1.0,  # max_impulse
+        )
+
+    def prone(self):
+        # Bend legs
+        calfDofs = [2, 5, 8, 11]
+        pose = np.zeros(12, dtype=np.float32)
+        for dof in calfDofs:
+            pose[dof] = self.task_config.START.CALF  # second joint
+            pose[dof - 1] = self.task_config.START.THIGH  # first joint
+
+        # Snap joints kinematically
+        self.robot_id.joint_positions = pose
+
+        # Make motor controllers maintain this position
+        for idx, p in enumerate(pose):
+            self.robot_id.update_joint_motor(idx, self._new_jms(p))
+        
+    def stand(self):
+        # Bend legs
+        pose = np.array([0, 0.432, -0.77] * 4)
+
+        # Snap joints kinematically
+        self.robot_id.joint_positions = pose
+
+        # Make motor controllers maintain this position
+        for idx, p in enumerate(pose):
+            self.robot_id.update_joint_motor(idx, self._new_jms(p))
+
+    def reset(self):
+        # Zero out the link and root velocities
+        self.robot_id.clear_joint_states()
+        self.robot_id.root_angular_velocity = mn.Vector3(0.0, 0.0, 0.0)
+        self.robot_id.root_linear_velocity = mn.Vector3(0.0, 0.0, 0.0)
+
+        # Roll robot 90 deg
+        base_transform = mn.Matrix4.rotation(
+            mn.Rad(np.deg2rad(-90)), mn.Vector3(1.0, 0.0, 0.0)
+        )
+
+        # Position above center of platform
+        base_transform.translation = (
+            mn.Vector3(0.0, 0.8, 0.0)
+            if self.fixed_base
+            else mn.Vector3(0.0, 0.3, 0.0)
+        )
+        self.robot_id.transformation = base_transform
+
+        # Reset all jms
+        base_jms = self._new_jms(0)
+        for i in range(12):
+            self.robot_id.update_joint_motor(i, base_jms)
+
+        self._set_joint_type_pos("thigh", self.task_config.START.THIGH)
+        self._set_joint_type_pos("calf", self.task_config.START.CALF)
+
+    def _set_joint_type_pos(self, joint_type, joint_pos):
+        """Sets all joints of a given type to a given position
+
+        Args:
+            joint_type: type of joint ie hip, thigh or calf
+            joint_pos: position to set these joints to
+        """
+        for idx, joint_name in enumerate(self.jmsIdxToJoint):
+            if joint_type in joint_name:
+                self.robot_id.update_joint_motor(idx, self._new_jms(joint_pos))
+
+    def add_jms_pos(self, joint_pos):
+        """
+        Updates existing joint positions by adding each position in array of
+        joint_positions
+        Args
+            joint_pos: array of delta joint positions
+        """
+        for i, new_pos in enumerate(joint_pos):
+            jms = self._jms_copy(self.robot_id.get_joint_motor_settings(i))
+            jms.position_target = np.clip(
+                wrap_heading(jms.position_target + new_pos),
+                self.joint_limits_lower[i],
+                self.joint_limits_upper[i],
+            )
+            self.robot_id.update_joint_motor(i, jms)
+
+    def get_rpy(self):
+        """Given a numpy quaternion we'll return the roll pitch yaw
+
+        Returns:
+            rpy: tuple of roll, pitch yaw
+        """
+        quat = self.robot_id.rotation.normalized()
+        undo_rot = mn.Quaternion(
+            ((np.sin(np.deg2rad(45)), 0.0, 0.0), np.cos(np.deg2rad(45)))
+        ).normalized()
+        quat = quat * undo_rot
+
+        x, y, z = quat.vector
+        w = quat.scalar
+
+        roll, pitch, yaw = self._euler_from_quaternion(x, y, z, w)
+        rpy = wrap_heading(np.array([roll, pitch, yaw]))
+        return rpy
+
+    def _euler_from_quaternion(self, x, y, z, w):
+        """
+        Convert a quaternion into euler angles (roll, yaw, pitch)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+
+        return roll_x, -yaw_z, pitch_y  # in radians
--- habitat_baselines/common/environments.py
+++ habitat_baselines/common/environments.py
@@ -29,8 +29,9 @@ from habitat_sim.physics import JointMotorSettings
 from copy import copy
 import math
 import torch
+import cv2
 from collections import defaultdict, OrderedDict
-
+from habitat_baselines.common.aliengo import AlienGo
 from habitat.utils.geometry_utils import wrap_heading
 
 
@@ -45,206 +46,22 @@ def get_env_class(env_name: str) -> Type[habitat.RLEnv]:
     """
     return baseline_registry.get_env(env_name)
 
+@baseline_registry.register_env(name="EnergyLocomotionRLEnv")
+class EnergyLocomotionRLEnv(habitat.RLEnv):
+    def __init__(self, config: Config, render=False, *args, **kwargs):
+        super().__init__(config, render, args=args, kwargs=kwargs) #not sure this is correct
 
-class AlienGo:
-    def __init__(self, robot_id, sim, fixed_base, task_config):
-        self.robot_id = robot_id
-        self._sim = sim
-        self.fixed_base = fixed_base
-        self.task_config = task_config
-
-        self.jmsIdxToJoint = [
-            "FL_hip",
-            "FL_thigh",
-            "FL_calf",
-            "FR_hip",
-            "FR_thigh",
-            "FR_calf",
-            "RL_hip",
-            "RL_thigh",
-            "RL_calf",
-            "RR_hip",
-            "RR_thigh",
-            "RR_calf",
-        ]
-
-        # joint position limits
-        self.joint_limits_lower = np.array(
-            [-0.1, -np.pi / 3, -5 / 6 * np.pi] * 4
-        )
-        self.joint_limits_upper = np.array([0.1, np.pi / 2.1, -np.pi / 4] * 4)
-
-    @property
-    def height(self):
-        # Translation is [y, z, x]
-        return self.robot_id.rigid_state.translation[1]
-
-    @property
-    def joint_velocities(self) -> np.ndarray:
-        return np.array(self.robot_id.joint_velocities, dtype=np.float32)
-
-    @property
-    def joint_positions(self) -> np.ndarray:
-        return np.array(self.robot_id.joint_positions, dtype=np.float32)
-
-    def set_joint_positions(self, pose):
-        """This is kinematic! Not dynamic."""
-        self.robot_id.joint_positions = wrap_heading(pose)
-
-    def get_feet_contacts(self):
-        """THIS ASSUMES THAT THERE IS ONLY ONE ROBOT IN THE SIM
-        Returns np.array size 4, either 1s or 0s, for FL FR RL RR feet.
-        """
-        contacts = self._sim.get_physics_contact_points()
-        contacting_feet = set()
-        for c in contacts:
-            for link in [c.link_id_a, c.link_id_b]:
-                contacting_feet.add(self.robot_id.get_link_name(link))
-        return np.array(
-            [
-                1 if foot in contacting_feet else 0
-                for foot in ["FL_foot", "FR_foot", "RL_foot", "RR_foot"]
-            ]
-        )
-
-    def _jms_copy(self, jms):
-        """Returns a deep copy of a jms
-
-        Args:
-            jms: the jms to copy
-        """
-        return JointMotorSettings(
-            jms.position_target,
-            jms.position_gain,
-            jms.velocity_target,
-            jms.velocity_gain,
-            jms.max_impulse,
-        )
-
-    def _new_jms(self, pos):
-        """Returns a new jms with default settings at a given position
-
-        Args:
-            pos: the new position to set to
-        """
-        return JointMotorSettings(
-            pos,  # position_target
-            0.6,  # position_gain
-            0.0,  # velocity_target
-            1.5,  # velocity_gain
-            1.0,  # max_impulse
-        )
-
-    def prone(self):
-        # Bend legs
-        calfDofs = [2, 5, 8, 11]
-        pose = np.zeros(12, dtype=np.float32)
-        for dof in calfDofs:
-            pose[dof] = self.task_config.START.CALF  # second joint
-            pose[dof - 1] = self.task_config.START.THIGH  # first joint
-
-        # Snap joints kinematically
-        self.robot_id.joint_positions = pose
-
-        # Make motor controllers maintain this position
-        for idx, p in enumerate(pose):
-            self.robot_id.update_joint_motor(idx, self._new_jms(p))
-
-    def reset(self):
-        # Zero out the link and root velocities
-        self.robot_id.clear_joint_states()
-        self.robot_id.root_angular_velocity = mn.Vector3(0.0, 0.0, 0.0)
-        self.robot_id.root_linear_velocity = mn.Vector3(0.0, 0.0, 0.0)
-
-        # Roll robot 90 deg
-        base_transform = mn.Matrix4.rotation(
-            mn.Rad(np.deg2rad(-90)), mn.Vector3(1.0, 0.0, 0.0)
-        )
-
-        # Position above center of platform
-        base_transform.translation = (
-            mn.Vector3(0.0, 0.8, 0.0)
-            if self.fixed_base
-            else mn.Vector3(0.0, 0.3, 0.0)
-        )
-        self.robot_id.transformation = base_transform
-
-        # Reset all jms
-        base_jms = self._new_jms(0)
-        for i in range(12):
-            self.robot_id.update_joint_motor(i, base_jms)
-
-        self._set_joint_type_pos("thigh", self.task_config.START.THIGH)
-        self._set_joint_type_pos("calf", self.task_config.START.CALF)
-
-    def _set_joint_type_pos(self, joint_type, joint_pos):
-        """Sets all joints of a given type to a given position
-
-        Args:
-            joint_type: type of joint ie hip, thigh or calf
-            joint_pos: position to set these joints to
-        """
-        for idx, joint_name in enumerate(self.jmsIdxToJoint):
-            if joint_type in joint_name:
-                self.robot_id.update_joint_motor(idx, self._new_jms(joint_pos))
-
-    def add_jms_pos(self, joint_pos):
-        """
-        Updates existing joint positions by adding each position in array of
-        joint_positions
-        Args
-            joint_pos: array of delta joint positions
-        """
-        for i, new_pos in enumerate(joint_pos):
-            jms = self._jms_copy(self.robot_id.get_joint_motor_settings(i))
-            jms.position_target = np.clip(
-                wrap_heading(jms.position_target + new_pos),
-                self.joint_limits_lower[i],
-                self.joint_limits_upper[i],
-            )
-            self.robot_id.update_joint_motor(i, jms)
-
-    def get_rpy(self):
-        """Given a numpy quaternion we'll return the roll pitch yaw
-
-        Returns:
-            rpy: tuple of roll, pitch yaw
-        """
-        quat = self.robot_id.rotation.normalized()
-        undo_rot = mn.Quaternion(
-            ((np.sin(np.deg2rad(45)), 0.0, 0.0), np.cos(np.deg2rad(45)))
-        ).normalized()
-        quat = quat * undo_rot
-
-        x, y, z = quat.vector
-        w = quat.scalar
-
-        roll, pitch, yaw = self._euler_from_quaternion(x, y, z, w)
-        rpy = wrap_heading(np.array([roll, pitch, yaw]))
-        return rpy
-
-    def _euler_from_quaternion(self, x, y, z, w):
-        """
-        Convert a quaternion into euler angles (roll, yaw, pitch)
-        roll is rotation around x in radians (counterclockwise)
-        pitch is rotation around y in radians (counterclockwise)
-        yaw is rotation around z in radians (counterclockwise)
-        """
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-
-        return roll_x, -yaw_z, pitch_y  # in radians
-
+    def get_reward_terms(self, observations) -> np.array:
+        reward_terms = OrderedDict()
+        # add each reward term here
+    
+    def get_observations(self):
+        return {
+            "joint_pos": self.robot.joint_positions,
+            "joint_vel": self.robot.joint_velocities,
+            "euler_rot": self.robot.get_rpy(),
+            "feet_contact": self.robot.get_feet_contacts(),
+        }
 
 @baseline_registry.register_env(name="LocomotionRLEnv")
 class LocomotionRLEnv(habitat.RLEnv):
@@ -305,8 +122,11 @@ class LocomotionRLEnv(habitat.RLEnv):
         # Place agent (for now, this is just a camera)
         self._place_agent()
 
+        # Debug
+        self.video_dir = config.VIDEO_DIR
+
         # Load the robot into the sim
-        self.fixed_base = False
+        self.fixed_base = self.config.TASK_CONFIG.DEBUG.FIXED_BASE
         ao_mgr = self._sim.get_articulated_object_manager()
         robot_id = ao_mgr.add_articulated_object_from_urdf(
             self.sim_config.ROBOT_URDF, fixed_base=self.fixed_base
@@ -362,18 +182,18 @@ class LocomotionRLEnv(habitat.RLEnv):
     def get_reward_terms(self, observations) -> np.array:
         reward_terms = OrderedDict()
 
-        # Penalize roll and pitch
-        reward_terms["roll_pitch_penalty"] = -np.abs(
-            observations["euler_rot"][:2]
-        )
+        # # Penalize roll and pitch
+        # reward_terms["roll_pitch_penalty"] = -np.abs(
+        #     observations["euler_rot"][:2]
+        # )
 
-        # Penalize non-contacting feet
-        reward_terms["contact_feet_penalty"] = observations["feet_contact"] - 1
+        # # Penalize non-contacting feet
+        # reward_terms["contact_feet_penalty"] = observations["feet_contact"] - 1
 
-        # Penalize deviation from desired height
-        reward_terms["height_penalty"] = [
-            -np.abs(self.robot.height - self.goal_height)
-        ]
+        # # Penalize deviation from desired height
+        # reward_terms["height_penalty"] = [
+        #     -np.abs(self.robot.height - self.goal_height)
+        # ]
 
         # Penalize deviations from nominal standing pose (MSE)
         reward_terms["imitation_reward"] = (
@@ -423,8 +243,8 @@ class LocomotionRLEnv(habitat.RLEnv):
         # Update current state
         self.robot.add_jms_pos(deltas)
         self._sim.step_physics(1.0 / self.sim_hz)
-        if self.render:
-            self.viz_buffer.append(self._sim.get_sensor_observations())
+        
+
 
         # Return observations (error for each knob)
         observations = self.get_observations()
@@ -433,17 +253,32 @@ class LocomotionRLEnv(habitat.RLEnv):
         reward_terms = self.get_reward_terms(observations)
         reward = sum(reward_terms)
 
+        # Text on Screen
+        if self.render:
+            self.viz_buffer.append(self._sim.get_sensor_observations())
+            # print(self.viz_buffer[-1])
+            self.viz_buffer[-1]["rgba_camera"] = cv2.putText(
+                self.viz_buffer[-1]["rgba_camera"], #numpy array on which text is written
+                f"Reward: {reward}", #text
+                (20, 20), #position at which writing has to start
+                cv2.FONT_HERSHEY_SIMPLEX, #font family
+                1, #font size
+                (256, 0, 0, 255), #font color
+                3#font stroke
+            )
+
         # Check termination conditions
         success = abs(self.robot.height - self.goal_height) < 0.05
         self.num_steps += 1
         done = self.num_steps == self._max_episode_steps
-        if done:
+        if done and self.render:
             vut.make_video(
                 self.viz_buffer,
                 "rgba_camera",
                 "color",
-                "test.mp4",
+                os.path.join(self.video_dir, "vid{rand}.mp4".format(rand=np.random.randint(0, 1e6))),
                 open_vid=False,
+                fps=self.sim_hz
             )
 
         # Populate info for tensorboard
@@ -503,6 +338,7 @@ class LocomotionRLEnv(habitat.RLEnv):
             cube_handle
         )
         cube_template_cpy.scale = np.array([5.0, 0.2, 5.0])
+        cube_template_cpy.friction_coefficient = 0.3
         obj_template_mgr.register_template(cube_template_cpy)
         rigid_obj_mgr = sim.get_rigid_object_manager()
         ground_plane = rigid_obj_mgr.add_object_by_template_handle(cube_handle)
